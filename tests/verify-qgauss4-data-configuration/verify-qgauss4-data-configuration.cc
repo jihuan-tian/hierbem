d576d017dd76856e35fe5df350d21245bfe8978b
Jihuan Tian <jihuan_tian@hotmail.com>
2023-03-23 15:23:16 +0800

    1. Remove the argument ~is_scratch_data_for_kx_uncalculated~ from ~sauter_assemble_on_one_pair_of_cells~, ~permute_dofs_and_mapping_support_points_for_sauter_quad~. 2. Migrate ~ky_cell_iter->get_dof_indices~ outside of ~permute_dofs_and_mapping_support_points_for_sauter_quad~ and into ~sauter_assemble_on_one_pair_of_cells~, two ~sauter_assemble_on_one_pair_of_dofs~.

---
+++ tests/verify-qgauss4-data-configuration/verify-qgauss4-data-configuration.cc
@@ -0,0 +1,8 @@
+/**
+ * @file verify-qgauss4-data-configuration.cc
+ * @brief 
+ *
+ * @ingroup testers
+ * @author Jihuan Tian
+ * @date 2023-03-22
+ */
---
+++ tests/verify-sauter-quad-parametric-transform-common-edge/verify-sauter-quad-parametric-transform-common-edge.cu
@@ -0,0 +1,8 @@
+/**
+ * @file verify-sauter-quad-parametric-transform-common-edge.cu
+ * @brief 
+ *
+ * @ingroup testers
+ * @author Jihuan Tian
+ * @date 2023-03-22
+ */
