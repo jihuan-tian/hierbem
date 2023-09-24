cfe82092d04f2c1aa25d91c0b5abb6e5695c1817
Jihuan Tian <jihuan_tian@hotmail.com>
2023-09-24 11:19:54 +0800

    1. Add CMake enabler variables HBEM_ENABLE_VALGRIND_PROFILE and HBEM_ENABLE_NSIGHT_PROFILE. 2. Refine the scripts for generating a test program template: scripts/init-test.sh.in and scripts/init-test-cuda.sh.in. 3. Create an independent file UnitTests.txt which contains all the test programs. It will be included in the top level CMakeLists.txt and wrapped between the checking of mixed build mode.

--- scripts/call-init-test-cuda.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# N.B. This script should be called within the directory of the test to be initialized.
-# The first argument is the build_type.
-../../scripts/init-test-cuda.sh $(basename $(pwd)) $1
--- scripts/call-init-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# N.B. This script should be called within the directory of the test to be initialized.
-# The first argument is the build_type.
-../../scripts/init-test.sh $(basename $(pwd)) $1
