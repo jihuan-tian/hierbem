db054cea83a44b6bfb1b3a81bab29fed391aecbb
Jihuan Tian <jihuan_tian@hotmail.com>
2024-01-30 20:34:23 +0800

    Add process scripts for test cases in `tests/functions/laplace-bem`

---
+++ tests/functions/laplace-bem/dirichlet-hmatrix-two-spheres/process.m
@@ -0,0 +1,9 @@
+clear all;
+## Load reference solution.
+load "/home/jihuan/Projects/hierbem/tests/functions/laplace-bem/dirichlet-hmatrix-two-spheres/reference.output";
+solution_ref = solution;
+## Load computation solution
+load "dirichlet-hmatrix-two-spheres.output";
+
+solution_l2_rel_err = norm(solution - solution_ref) / norm(solution_ref)
+solution_inf_rel_err = norm(solution - solution_ref, Inf) / norm(solution_ref, Inf)
