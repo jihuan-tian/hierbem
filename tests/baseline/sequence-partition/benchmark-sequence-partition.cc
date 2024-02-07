dfdb0ffef72a300dcaab4820d22e1e8e11bac169
chaoslawful <chaoslawful@gmail.com>
2024-02-07 18:08:10 +0800

    Implement sequence partition algorithm

---
+++ tests/baseline/sequence-partition/benchmark-sequence-partition.cc
@@ -0,0 +1,78 @@
+#include <chrono>
+#include <execution>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <utility>
+#include <vector>
+
+#include "sequence_partition/sequence_partition.h"
+
+int
+main()
+{
+  std::cout << "Generating random costs" << std::endl;
+  std::random_device               rd;
+  std::mt19937                     gen(rd());
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+
+  int64_t n = 1000000;
+  int64_t p = 256;
+
+  // std::vector<double> task_costs(n);
+  // for (auto &num : task_costs)
+  //   {
+  //     num = dis(gen);
+  //   }
+
+  // auto cost_func = [&task_costs](int64_t i, int64_t j) -> double {
+  //   return std::accumulate(task_costs.begin() + i, task_costs.begin() + j,
+  //   0.0);
+  // };
+
+  auto cost_func = [](int64_t i, int64_t j) -> double { return j - i + 1.0; };
+
+  std::cout << "Optimizing partitioning" << std::endl;
+  SequencePartitioner<decltype(cost_func)> sp(n, p, cost_func);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  sp.partition();
+  auto end = std::chrono::high_resolution_clock::now();
+
+  double minmax_cost = sp.get_minmax_cost();
+  std::cout << "Minimum maximum interval cost: " << minmax_cost << std::endl;
+  std::cout << "Time: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                     start)
+                 .count()
+            << " ms" << std::endl;
+
+  std::vector<std::pair<int64_t, int64_t>> parts;
+  start = std::chrono::high_resolution_clock::now();
+  sp.get_partitions(parts);
+  end = std::chrono::high_resolution_clock::now();
+  std::cout << "Traverse partitions in "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                     start)
+                 .count()
+            << " ms: " << std::endl;
+
+  double max_cost = 0.0;
+  for (int64_t i = 0; i < p; i++)
+    {
+      int64_t left          = parts[i].first;
+      int64_t right         = parts[i].second;
+      double  interval_cost = cost_func(left, right);
+#if 0
+      std::cout << "[" << left << "," << right << "]: " << interval_cost
+                << std::endl;
+#endif
+      if (interval_cost > max_cost)
+        {
+          max_cost = interval_cost;
+        }
+    }
+  std::cout << "maximum cost: " << max_cost << std::endl;
+
+  return 0;
+}
---
+++ tests/units/sequence_partition/verify-algorithm/verify-algorithm.cc
@@ -0,0 +1,136 @@
+/**
+ * @file verify-algorithm.cc
+ * @brief
+ *
+ * @ingroup testers
+ * @author
+ * @date 2024-02-04
+ */
+#include <catch2/catch_all.hpp>
+
+#include <random>
+#include <vector>
+
+#include "sequence_partition/sequence_partition.h"
+
+using namespace Catch::Matchers;
+
+static constexpr int FUZZING_TIMES = 5;
+static constexpr int FIXED_SEED    = 123456;
+
+std::vector<double>
+generate_random_task_costs(int seed, int n)
+{
+  std::mt19937                     gen(seed);
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+
+  std::vector<double> task_costs(n);
+  for (auto &num : task_costs)
+    {
+      num = dis(gen);
+    }
+  return task_costs;
+}
+
+TEST_CASE("Sequence Partition Basic Test", "[seq_part]")
+{
+  int  n            = 40;
+  int  n_partitions = 5;
+  auto trial_no     = GENERATE(range(0, FUZZING_TIMES));
+  SECTION(std::string("trial #") + std::to_string(trial_no))
+  {
+    auto task_costs = generate_random_task_costs(FIXED_SEED, n);
+    auto cost_func  = [&task_costs](int i, int j) -> double {
+      double sum = 0.0;
+      for (int k = i; k <= j; k++)
+        {
+          sum += task_costs[k];
+        }
+      return sum;
+    };
+
+    SequencePartitioner<decltype(cost_func)> sp(n, n_partitions, cost_func);
+    sp.partition();
+
+    double minmax_cost = sp.get_minmax_cost();
+    CHECK(minmax_cost > 0.0);
+  }
+}
+
+TEST_CASE("Partition minmax cost non-increasing to the number of partitions",
+          "[seq_part]")
+{
+  int n              = 40;
+  int max_partitions = 10;
+
+  auto   task_costs = generate_random_task_costs(FIXED_SEED, n);
+  double prev_cost  = 0.0;
+  for (int p = 1; p <= max_partitions; p++)
+    {
+      auto cost_func = [&task_costs](int i, int j) -> double {
+        double sum = 0.0;
+        for (int k = i; k <= j; k++)
+          {
+            sum += task_costs[k];
+          }
+        return sum;
+      };
+      SequencePartitioner<decltype(cost_func)> sp(n, p, cost_func);
+      sp.partition();
+
+      double minmax_cost = sp.get_minmax_cost();
+      CHECK(minmax_cost > 0.0);
+
+      INFO("Minimum maximum interval cost for "
+           << p << " partitions: " << minmax_cost);
+
+      // Non-increasing property test
+      if (prev_cost > 0.0)
+        {
+          CHECK(minmax_cost <= prev_cost);
+        }
+      else
+        {
+          prev_cost = minmax_cost;
+        }
+    }
+}
+
+TEST_CASE("Non-empty partition and correct minmax cost test", "[seq_part]")
+{
+  int  n        = 40;
+  int  p        = 10;
+  auto trial_no = GENERATE(range(0, FUZZING_TIMES));
+
+  SECTION(std::string("trial #") + std::to_string(trial_no))
+  {
+    auto task_costs = generate_random_task_costs(FIXED_SEED, n);
+    auto cost_func  = [&task_costs](int i, int j) -> double {
+      double sum = 0.0;
+      for (int k = i; k <= j; k++)
+        {
+          sum += task_costs[k];
+        }
+      return sum;
+    };
+
+    SequencePartitioner<decltype(cost_func)> sp(n, p, cost_func);
+    sp.partition();
+
+    double pred_cost   = sp.get_minmax_cost();
+    double actual_cost = 0.0;
+
+    std::vector<std::pair<int64_t, int64_t>> parts;
+    sp.get_partitions(parts);
+
+    for (auto &part : parts)
+      {
+        // Make sure each partition is not empty
+        CHECK(part.first <= part.second);
+        actual_cost = std::max(actual_cost, cost_func(part.first, part.second));
+      }
+
+    // Make sure the actual cost is the same as the predicted cost
+    REQUIRE_THAT(actual_cost, WithinAbs(pred_cost, 1e-6));
+  }
+}
