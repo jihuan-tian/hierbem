266feafff6a3815984597f41f1630d9ccd91e967
Jihuan Tian <jihuan_tian@hotmail.com>
2024-08-05 21:01:21 +0800

    Update output files

--- tests/units/build-surface-to-line-topology/build-surface-to-line-topology.cc
+++ tests/units/build-surface-to-line-topology/build-surface-to-line-topology.cc
@@ -18,12 +18,24 @@ main(int argc, const char *argv[])
   (void)argc;
   (void)argv;
 
-  DDMEfield<1, 3> efield;
-  efield.read_subdomain_topology(HBEM_TEST_MODEL_DIR
-                                 "circle-immersed-in-two-squares.geo",
-                                 HBEM_TEST_MODEL_DIR
-                                 "circle-immersed-in-two-squares.msh");
-  efield.get_subdomain_topology().print(std::cout);
+  {
+    DDMEfield<1, 3> efield;
+    efield.read_subdomain_topology(HBEM_TEST_MODEL_DIR
+                                   "circle-immersed-in-two-squares.geo",
+                                   HBEM_TEST_MODEL_DIR
+                                   "circle-immersed-in-two-squares.msh");
+    efield.get_subdomain_topology().print(std::cout);
+  }
+
+  {
+    DDMEfield<1, 3> efield;
+    efield.read_subdomain_topology(
+      HBEM_TEST_MODEL_DIR
+      "circle-immersed-in-two-squares-different-surface-orientations.geo",
+      HBEM_TEST_MODEL_DIR
+      "circle-immersed-in-two-squares-different-surface-orientations.msh");
+    efield.get_subdomain_topology().print(std::cout);
+  }
 
   return 0;
 }
