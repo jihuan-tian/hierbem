c945a175aff0a97c98d15766191c711f5896189f
chaoslawful <chaoslawful@gmail.com>
2024-10-28 23:02:38 +0800

    add more reflectcpp demo cases

--- tests/demos/reflectcpp-usage/reflectcpp-usage.cc
+++ tests/demos/reflectcpp-usage/reflectcpp-usage.cc
@@ -48,6 +48,121 @@ struct ConfigHierBEM
   ConfigBEM     bem     = ConfigBEM{};
 };
 
+TEST_CASE("Sanity: json schema desc", "[json][demo]")
+{
+  struct Config
+  {
+    rfl::Description<"The project name should contains only alphanumerics",
+                     std::string>
+      proj_name;
+    rfl::Description<"The finite space dimension, should be 1~3", std::uint32_t>
+      space_dim;
+  };
+  auto json_schema = rfl::json::to_schema<Config>(rfl::json::pretty);
+  std::cout << json_schema << std::endl;
+}
+
+TEST_CASE("Sanity: validator", "[toml][demo]")
+{
+  using ProjectName = rfl::Pattern<R"([a-zA-Z0-9_]+)", "ProjectName">;
+  using SpaceDim =
+    rfl::Validator<std::uint32_t, rfl::AllOf<rfl::Minimum<1>, rfl::Maximum<3>>>;
+  struct Config
+  {
+    ProjectName name;
+    SpaceDim    dim;
+  };
+  auto       toml_str = R"(
+    name = "hierbem"
+    dim = 3
+  )";
+  const auto config   = rfl::toml::read<Config>(toml_str).value();
+  REQUIRE(config.name == "hierbem");
+  REQUIRE(config.dim == 3);
+  auto err_toml_str = R"(
+    name = "hierbem"
+    dim = 0
+  )";
+  REQUIRE_THROWS(rfl::toml::read<Config>(err_toml_str).value());
+  auto err_toml_str1 = R"(
+    name = "%*%"
+    dim = 2
+  )";
+  REQUIRE_THROWS(rfl::toml::read<Config>(err_toml_str1).value());
+}
+
+enum class Color
+{
+  Green = 2,
+  Red   = 1,
+  Blue  = 4,
+  Black = 8
+};
+inline Color
+operator|(Color a, Color b)
+{
+  return static_cast<Color>(static_cast<int>(a) | static_cast<int>(b));
+}
+
+TEST_CASE("Sanity: scoped enum", "[toml][demo]")
+{
+  enum class ProblemType
+  {
+    Interior,
+    Exterior
+  };
+
+  struct Config
+  {
+    ProblemType          problem_type;
+    std::optional<Color> color;
+  };
+
+  std::string toml_str = R"(
+    problem_type = "Interior"
+  )";
+  const auto  config   = rfl::toml::read<Config>(toml_str).value();
+  REQUIRE(config.problem_type == ProblemType::Interior);
+
+  std::string toml_str1 = R"(
+    problem_type = "Unknown"
+  )";
+  REQUIRE_THROWS(rfl::toml::read<Config>(toml_str1).value());
+
+  const auto conf_obj  = Config{.problem_type = ProblemType::Exterior,
+                                .color        = Color::Red | Color::Green};
+  const auto toml_str2 = rfl::toml::write(conf_obj);
+  std::cout << toml_str2 << std::endl;
+
+  const auto conf_obj2 = rfl::toml::read<Config>(toml_str2).value();
+  REQUIRE(conf_obj2.problem_type == ProblemType::Exterior);
+  REQUIRE(conf_obj2.color.has_value());
+  REQUIRE(conf_obj2.color.value() == (Color::Red | Color::Green));
+}
+
+TEST_CASE("Sanity: string literals", "[toml][demo]")
+{
+  using ProblemType = rfl::Literal<"interior", "exterior">;
+  struct Config
+  {
+    ProblemType problem_type;
+  };
+
+  std::string toml_str = R"(
+    problem_type = "interior"
+  )";
+  const auto  config   = rfl::toml::read<Config>(toml_str).value();
+  std::cout << config.problem_type.name() << std::endl;
+  std::cout << config.problem_type.value() << std::endl;
+  REQUIRE(config.problem_type == "interior");
+  REQUIRE(config.problem_type == ProblemType::value_of<"interior">());
+
+  std::string toml_str_err = R"(
+    problem_type = "unknown"
+  )";
+  REQUIRE_THROWS(rfl::toml::read<Config>(toml_str_err).value());
+}
+
 TEST_CASE("Sanity: processors", "[toml][demo]")
 {
   struct Person
@@ -84,6 +199,17 @@ TEST_CASE("Sanity: processors", "[toml][demo]")
 
   REQUIRE(conf.a == 1);
   REQUIRE(conf.b == "bar");
+
+  std::string toml_str1 = R"(
+    a = 1
+    bb = "foo"
+  )";
+  REQUIRE_NOTHROW(
+    rfl::toml::read<SomeConf, rfl::DefaultIfMissing>(toml_str1).value());
+  REQUIRE_THROWS(
+    rfl::toml::read<SomeConf, rfl::DefaultIfMissing, rfl::NoExtraFields>(
+      toml_str1)
+      .value());
 }
 
 TEST_CASE("Sanity: flatten", "[toml][demo]")
